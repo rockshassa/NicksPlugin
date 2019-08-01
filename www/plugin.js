d057830048a641c22fad1806ee8e80e738bc7820
Nicholas Galasso <nick.galasso@animoto.com>
2019-08-01 16:37:24 -0400

    fix

--- www/plugin.js
+++ www/plugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'nicksPlugin', 'coolMethod', [arg0]);
+exports.echo = function (arg0, success, error) {
+    exec(success, error, 'nicksPlugin', 'echo', [arg0]);
 };
